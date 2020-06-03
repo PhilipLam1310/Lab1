d25a6033d285ca5466a60bea6b90f3af291ee58f
lmphilip13 <lmphilip13@SE140432>
2020-06-03 08:21:53 +0700

    first commit

---
+++ Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Students
+    {
+        protected String _uuid;
+        protected String _fullname;
+        protected String _birthday;
+        protected Boolean _gender;
+
+        public String UUID { get { return _uuid; } }
+        public String FullName { get { return _fullname; } }
+        public String Birthday { get { return _birthday; } }
+        public Boolean Gender { get { return _gender; } }
+
+
+    }
+}
